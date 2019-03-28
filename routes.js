65fad60cbfb351cba59b16854e8fa8712a8f0a40
Miłosz Szatkowski <milosz.dawid.szatkowski@gmail.com>
2019-03-28 15:19:43 +0000

    d
    d

--- routes.js
+++
@@ -1,9 +0,0 @@
-const routes = require( 'next-routes' );
-
-// Setup router.
-module.exports = routes()
-  .add( 'index', '/' )
-  .add( 'products', '/products/:slug')
-  .add( 'single', '/products/details/:slug' )
-  .add( 'contact', '/contact' )
-  .add( 'about', '/about' );
